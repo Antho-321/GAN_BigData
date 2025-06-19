c5e3135ac08c0fd55d33a69775aead3ae8319145
lizbethquilumba <jlquilumbad@utn.edu.ec>
2025-06-18 19:10:29 -0500

    reduzco bath size

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 LATENT_DIM = 100
 IMG_SHAPE = (28, 28, 1)
 EPOCHS = 2000
-BATCH_SIZE = 64
+BATCH_SIZE = 8
 SAVE_INTERVAL = 200
 LEARNING_RATE = 0.0002 # Adam optimizer learning rate
 ADAM_BETA_1 = 0.5    # Adam optimizer beta1
@@ -15,7 +15,7 @@ ADAM_BETA_1 = 0.5    # Adam optimizer beta1
 # --- Configuración para la Métrica FID ---
 # El input_shape mínimo para InceptionV3 es 75x75
 INCEPTION_INPUT_SHAPE = (75, 75, 3)
-FID_BATCH_SIZE = 16
+FID_BATCH_SIZE = 8
 
 # --- Rutas y Directorios ---
 GENERATED_IMAGES_DIR = "/content/drive/MyDrive/Colab Notebooks/generated_images"
